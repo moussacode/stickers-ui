90d98be96043456bd76ce44aaa6670f7310d778c
moussacode <samouh269@gmail.com>
2026-08-07 15:36:35 +0000

    modification numero

--- src/app/config/site-config.ts
+++ src/app/config/site-config.ts
@@ -1,4 +1,4 @@
 /** Numéro WhatsApp du vendeur, format international sans le "+". */
-export const WHATSAPP_NUMBER = '221764795788';
+export const WHATSAPP_NUMBER = '221778740422';
 
 export const SITE_NAME = 'StickerBag';
